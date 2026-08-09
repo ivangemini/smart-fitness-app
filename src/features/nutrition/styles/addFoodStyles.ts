a026e03ff9d0928127e8a5b4477681ba8b2d44bd
ivangemini <ivangemini13@gmail.com>
2026-08-09 14:38:58 +0300

    ui: resolve adaptive glass in Add Food styles

--- src/features/nutrition/styles/addFoodStyles.ts
+++ src/features/nutrition/styles/addFoodStyles.ts
@@ -1,11 +1,16 @@
 import { Colors } from '@/constants/theme';
+import { resolveLiquidGlassPalette } from '@/theme/liquidGlass';
 
 import { createAddFoodBaseStyles } from './addFoodBaseStyles';
 import { createAddFoodScannerStyles } from './addFoodScannerStyles';
 import { createAddFoodSheetStyles } from './addFoodSheetStyles';
 
-export const createAddFoodStyles = (colors: typeof Colors.dark) => ({
-  ...createAddFoodBaseStyles(colors),
-  ...createAddFoodScannerStyles(colors),
-  ...createAddFoodSheetStyles(colors),
-});
+export const createAddFoodStyles = (colors: typeof Colors.light) => {
+  const glass = resolveLiquidGlassPalette(colors === Colors.light ? 'light' : 'dark');
+
+  return {
+    ...createAddFoodBaseStyles(colors, glass),
+    ...createAddFoodScannerStyles(colors, glass),
+    ...createAddFoodSheetStyles(colors, glass),
+  };
+};
