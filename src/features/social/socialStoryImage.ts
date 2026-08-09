361dde751344f8061af215f76f51a865769a4f42
ivangemini <ivangemini13@gmail.com>
2026-08-09 20:55:40 +0300

    Reuse managed image preparation for Stories

---
+++ src/features/social/socialStoryImage.ts
@@ -0,0 +1,10 @@
+export {
+  prepareSocialWorkoutPostImage as prepareSocialStoryImage,
+  recoverPendingSocialWorkoutPostImage as recoverPendingSocialStoryImage,
+  selectSocialWorkoutPostImage as selectSocialStoryImage,
+} from './socialWorkoutPostImage';
+
+export type {
+  PreparedSocialWorkoutPostImage as PreparedSocialStoryImage,
+  SelectedSocialWorkoutPostImage as SelectedSocialStoryImage,
+} from './socialWorkoutPostImage';
