962c80e2b228675396a7929af8da93b84d1e4a4c
ivangemini <ivangemini13@gmail.com>
2026-08-13 09:49:24 +0300

    Register multi-marker Lab trends screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -88,6 +88,7 @@ function RootNavigator() {
                   <Stack.Screen name="settings/social-profile" options={{ headerShown: false }} />
                   <Stack.Screen name="labs-compare" options={{ headerShown: false }} />
                   <Stack.Screen name="labs-compare-select" options={{ headerShown: false }} />
+                  <Stack.Screen name="labs-trends" options={{ headerShown: false }} />
                   <Stack.Screen name="labs-document/[documentId]" options={{ headerShown: false }} />
                   <Stack.Screen name="labs-marker/[markerId]" options={{ headerShown: false }} />
                   <Stack.Screen name="social/index" options={{ headerShown: false }} />
