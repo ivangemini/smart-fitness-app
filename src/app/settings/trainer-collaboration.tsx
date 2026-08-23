3c6f8bbd294f568a9a61b9845fe6c8909cbc75f1
ivangemini <ivangemini13@gmail.com>
2026-08-24 01:12:43 +0300

    feat(trainer): route collaboration settings screen

---
+++ src/app/settings/trainer-collaboration.tsx
@@ -0,0 +1,5 @@
+import { TrainerCollaborationScreen } from '@/features/trainer/TrainerCollaborationScreen';
+
+export default function TrainerCollaborationSettingsScreen() {
+  return <TrainerCollaborationScreen />;
+}
